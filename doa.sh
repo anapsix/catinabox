b37ad294d674e1202519d5b31dab8eddfe0fd163
Anastas Dancha <adancha@finmason.com>
2015-09-28 17:36:48 -0400

    ALIVE/DEAD

---
+++ doa.sh
@@ -0,0 +1,62 @@
+#!/bin/sh
+
+# get a randomish number
+NUM=$RANDOM
+
+# figure out last character index 
+let i=${#NUM}-1
+
+
+dead() {
+cat << EOM
+
+          /\    /\         
+         /) \__/ (\        
+   _     ~        ~        
+  { \._  - X    X -  _./ } 
+  /_"=-. -   ^^   - .-="_\ 
+   _  _.  - _~~_ -  =._ _  
+  (_'"_.-          -._"'_) 
+   {_"      RIP       "_} 
+
+EOM
+}
+
+alive1() {
+cat <<EOM
+
+          /\    /\  
+         /) \__/ (\ 
+ ------  ~        ~ 
+| MEOW | - o)  o) - 
+ ----- \ -   ^^   - 
+          - _~~_ -  
+
+EOM
+}
+
+alive2() {
+cat <<EOM
+          /\    /\  
+         /) \__/ (\ 
+ ------  ~        ~ 
+| MEOW | - ()  () | 
+ ----- \ -   ^^   - 
+          - _~~_ -  
+
+EOM
+}
+
+# check if cat is dead or alive
+case ${NUM:$i} in
+ [13579])
+   dead
+   exit 1
+ ;;
+ *)
+  alive1
+  exit 0
+ ;;
+esac
+
+#EOF
